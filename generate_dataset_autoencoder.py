f9d9614b0f5169a32c64cee571e2e3754f043106
donaldssh <donald.shenaj@studenti.unipd.it>
2020-12-29 12:44:35 +0100

    changed generation autoencoder dataset to surf 64

--- generate_dataset_autoencoder.py
+++ generate_dataset_autoencoder.py
@@ -7,9 +7,8 @@ def main(paths, out):
     
     descriptors = []
 
-    # Create SURF object with Hessian Threshold=600, 128 values (extended) 
-    surf = cv2.xfeatures2d.SURF_create(600)
-    surf.setExtended(True)
+    surf = cv2.xfeatures2d.SURF_create()
+    #surf.setExtended(True)
     
     for path in paths:
         for entry in os.scandir(path):
@@ -22,7 +21,6 @@ def main(paths, out):
                 kp, des = surf.detectAndCompute(img, None)
                 print(f"descriptors shape: {des.shape}")
                 
-                # keypoints.append(kp)
                 descriptors.append(des)
 
     descriptors = np.concatenate(descriptors, axis=0)
@@ -37,16 +35,20 @@ in binary composed by the 128 values of every surf descriptors in every image.
 Example:
 
 train_set:
-    python generate_dataset_autoencoder.py --paths ~/data/portelloDataset/ ~/data/castle-P19/ --out train_set.bin
+    python generate_dataset_autoencoder.py --paths ~/data/portelloDataset/ ~/data/castle-P30/images  --out train_set_64.bin
+    
+    Total descriptors shape: (723252, 64)
     
 test_set:
-    python generate_dataset_autoencoder.py --paths ~/data/fountain-P11/ ~/data/tisoDataset/ --out test_set.bin
+    python generate_dataset_autoencoder.py --paths ~/data/fountain-P11/images/ ~/data/tisoDataset --out test_set_64.bin
+
+    Total descriptors shape: (1174700, 64)
     
 """
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(
-            description="Create a csv dataset containing the surf descriptors of the given set of images")
+            description="Create a bin dataset containing the surf descriptors of the given set of images")
     
     parser.add_argument("--paths", nargs='+', required=True, help="paths to the datasets")
     parser.add_argument("--out", help="name of output dataset")
